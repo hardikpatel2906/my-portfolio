9f2d7dfc4639ff65aea568274c9a6c48508ebe8e
Hardik Patel <h.patel290695@gmail.com>
2024-11-28 22:09:25 -0500

    Update Contact me Page

--- src/components/GetInTouch.js
+++ src/components/GetInTouch.js
@@ -1,10 +1,16 @@
 const GetInTouch = () => {
+
+    const SendMessage = () => {
+        alert("Message Sent!")
+    }
+
+
     return (
         <>
             <div className="mx-auto max-w-4xl mt-6">
                 <h1 className="font-Jost text-5xl text-center">Contact me</h1>
                 <div className="flex justify-center mt-8">
-                    <form>
+                    <form onSubmit={SendMessage}>
                         <div class="grid gap-6 mb-6 md:grid-cols-2">
                             <div className="w-[300px]">
                                 <label for="full_name" class="block mb-2 text-lg font-medium text-gray-900 font-Jost">Full name</label>
@@ -12,22 +18,22 @@ const GetInTouch = () => {
                             </div>
                             <div className="w-[300px]">
                                 <label for="email" class="block mb-2 text-lg font-medium text-gray-900 font-Jost">Email</label>
-                                <input type="email" id="email" class="bg-gray-50 border border-gray-300 text-lg rounded-sm block w-full p-2.5" placeholder="Enter name" required />
+                                <input type="email" id="email" class="bg-gray-50 border border-gray-300 text-lg rounded-sm block w-full p-2.5" placeholder="Enter email" required />
                             </div>
                             <div className="w-[300px]">
                                 <label for="phone" class="block mb-2 text-lg font-medium text-gray-900 font-Jost">Phone</label>
-                                <input type="text" id="phone" class="bg-gray-50 border border-gray-300 text-lg rounded-sm block w-full p-2.5" placeholder="Enter name" required />
+                                <input type="text" id="phone" class="bg-gray-50 border border-gray-300 text-lg rounded-sm block w-full p-2.5" placeholder="Enter phone" required />
                             </div>
                             <div className="w-[300px]">
                                 <label for="subject" class="block mb-2 text-lg font-medium text-gray-900 font-Jost">Subject</label>
-                                <input type="text" id="subject" class="bg-gray-50 border border-gray-300 text-lg rounded-sm block w-full p-2.5" placeholder="Enter name" required />
+                                <input type="text" id="subject" class="bg-gray-50 border border-gray-300 text-lg rounded-sm block w-full p-2.5" placeholder="Enter subject" required />
                             </div>
                         </div>
                         <div className="w-[300px]">
                             <label for="message" class="block mb-2 text-lg font-medium text-gray-900 font-Jost">Message</label>
-                            <textarea type="text" id="message" class="bg-gray-50 border border-gray-300 text-lg rounded-sm block w-full p-2.5" placeholder="Enter name" required />
+                            <textarea type="text" id="message" class="bg-gray-50 border border-gray-300 text-lg rounded-sm block w-full p-2.5" placeholder="Enter brief message" required />
                         </div>
-                        <button className="bg-black rounded-md text-white p-5 font-Jost mt-10 text-lg" >Send Message</button>
+                        <button type="submit" className="bg-black rounded-md text-white p-5 font-Jost mt-10 text-lg">Send Message</button>
                     </form>
                 </div>
             </div>
