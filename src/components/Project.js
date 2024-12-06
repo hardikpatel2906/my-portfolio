1e4bdee4ef8b9d7194f50ebff24b268f2d53d9c0
Hardik Patel <h.patel290695@gmail.com>
2024-12-05 23:41:45 -0500

    Change Projects Page

--- src/components/Project.js
+++ src/components/Project.js
@@ -1,10 +1,18 @@
 const Project = () => {
+    const projectTech = ["ReactJS", "NodeJS", "ExpressJS", "MongoDB"]
     return (
         <>
             <div className="mx-auto max-w-5xl mt-6">
                 <h1 className="font-Jost text-5xl">My Projects</h1>
-                <h1 className="font-Jost text-3xl">1.</h1>
-                <h1 className="font-Jost text-3xl">GatherGro</h1>
+                <h1 className="font-Jost text-3xl mt-6">GatherGro</h1>
+                <h3 className="font-Jost text-xl">A web solution for the local farmers to sell local products online as well as connect with other localities near by. It also provides a plateform to exchange knowledge with each other.</h3>
+                <div className="flex flex-wrap gap-3 mt-2">
+                    {
+                        projectTech.map(tech => (
+                            <button className="bg-gray-700 text-white rounded-full px-4 py-2 text-lg font-Jost">{tech}</button>
+                        ))
+                    }
+                </div>
             </div>
         </>
     )
