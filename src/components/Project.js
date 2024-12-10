d8c83c378982a0ca2386fbb9aadfafff2b52dff4
Hardik Patel <h.patel290695@gmail.com>
2024-12-09 23:57:59 -0500

    project

--- src/components/Project.js
+++ src/components/Project.js
@@ -4,7 +4,7 @@ const Project = () => {
         <>
             <div className="mx-auto max-w-5xl mt-6">
                 <h1 className="font-Jost text-5xl">My Projects</h1>
-                <h1 className="font-Jost text-3xl mt-6">GatherGro</h1>
+                <h1 className="font-Jost text-3xl mt-6">1. GatherGro</h1>
                 <h3 className="font-Jost text-xl">A web solution for the local farmers to sell local products online as well as connect with other localities near by. It also provides a plateform to exchange knowledge with each other.</h3>
                 <div className="flex flex-wrap gap-3 mt-2">
                     {
